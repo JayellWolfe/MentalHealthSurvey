f23c3ae79b3f87e4d9413d46b634fafd12da204a
Jessica Williams <32785700+JayellWolfe@users.noreply.github.com>
2019-08-30 07:03:16 -0400

    Update index.py
    committed project code

--- pages/index.py
+++ pages/index.py
@@ -29,17 +29,40 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Value Proposition
+             2014 survey which measures the occurrence and prevalence of mental health disorders in the Tech workplace. 
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
+            People who had a family history of mental health disorders were more likely to be affected at work, if they felt that it would be difficult for them to take leave days. 
 
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
+            Women in the tech industries are much more likely to be affected at work by mental health disorders they younger they are. Where contrarily, men, are much more likely to be affected the older they are. 
 
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            Men and women who are not in the tech industry show an inverse pattern. Older women and younger aged men are more affected.      
+            
+            Being self-employed increases the chance of mental health disorders and with those disorders interfering with work. 
+
+            For male employees, being employed in the tech industry does not have an impact on a mental health disorder interfering with work. 
+
+            Women employees seem slightly more mentally healthy if they are employed in the tech industry 
+            
+            Mental health disorders are an impediment on performance and productivity. Everyone is affected differently and those with a family history of mental health disorders are more prone and vulnerable to mental health disorders and their impact on their work. 
+
+            As it is shown that employees who were under stricter leave policies at their workplace were more likely to suffer from a mental health disorders, a solution may be for companies to have better, more flexible, leave policies in place which would help lead to a reduction in mental health disorders having an impact on an employees work performance and productivity. 
+
+            When going over the true predictions versus the predictions, It shows that the confusion matrix told us that our model did not accurately predict the correct values. The number of true values that were true and true values that were false are nearly identical. False values that were true versus false values that were false were also nearly identical.  
+
+
+
+
+
+
+
+
+
+
+
 
             """
         ),
-        dcc.Link(dbc.Button('Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Button to Nowhere', color='primary'), href='/predictions')
     ],
     md=4,
 )
@@ -50,8 +73,41 @@ fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size
 
 column2 = dbc.Col(
     [
-        dcc.Graph(figure=fig),
+       html.Div(
+            [
+             html.Img(
+                            src=app.get_asset_url("../assets/download.png"),
+                            id="plotly-image",
+                            style={
+                                "height": "auto",
+                                "width": "auto",
+                                "margin-bottom": "25px",
+                            },
+                        ),
+               html.Img(
+                            src=app.get_asset_url("../assets/download2.png"),
+                            id="plotly-image",
+                            style={
+                                "height": "auto",
+                                "width": "auto",
+                                "margin-bottom": "25px",
+                            },
+                        ),
+               html.Img(
+                            src=app.get_asset_url("../assets/download3.png"),
+                            id="plotly-image",
+                            style={
+                                "height": "auto",
+                                "width": "auto",
+                                "margin-bottom": "25px",
+                            },
+                        ),
+               
+             
+            ]
+       )
     ]
+
 )
 
 layout = dbc.Row([column1, column2])
